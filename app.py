fe7ffbd0b71b17a45364c06fb91a46b1b94f2a5c
kfk <alessiocivitillo@gmail.com>
2012-03-06 23:42:04 +0100

    first

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, json, render_template
+import json
+
+app = Flask(__name__)
+
+app.config.update(DEBUG=True)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/json')
+def get_json():
+	d = {'a':'b','alessio':'civitillo'}
+	return json.dumps(d)
+
+if __name__=='__main__':
+	app.run()
